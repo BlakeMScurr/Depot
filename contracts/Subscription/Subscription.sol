ed71ad69775fcddec819710707c709dab314424e
blakemscurr <blakemscurr@gmail.com>
2021-09-09 19:34:27 +1200

    Replace locker code with vastly simpler ownership code.

--- contracts/Subscription/Subscription.sol
+++ contracts/Subscription/Subscription.sol
@@ -2,20 +2,21 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-import "./Locker.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 
 contract Subscription {
-    enum Model { Free, Lockup, Burn }
+    enum Model { Free, Membership, Burn }
 
     Model model;
-    Locker locker;
+    IERC20 erc20;
     address serverEthAddress;
     uint256 price;
     uint256 constant max_uint256 = 2**256 - 1;
 
-    constructor(address _serverEthAddress, address _locker, address _burner) {
+    constructor(address _serverEthAddress, address _erc20, address _burner) {
         serverEthAddress = _serverEthAddress;
-        locker = Locker(_locker);
+        erc20 = IERC20(_erc20);
         model = Model.Free;
         price = max_uint256;
     }
@@ -23,8 +24,8 @@ contract Subscription {
     function subscribed(address user) public view returns(bool) {
         if (model == Model.Free) {
             return true;
-        } else if (model == Model.Lockup) {
-            return locker.amountLocked(user) > price;
+        } else if (model == Model.Membership) {
+            return erc20.balanceOf(user) >= price;
         } else if (model == Model.Burn) {
             return false;
         }
@@ -42,10 +43,10 @@ contract Subscription {
     function updateModel() public {
         require(msg.sender == serverEthAddress, "Only the server can change the subscription model");
         require(model != Model.Burn);
-        if (model == Model.Lockup) {
+        if (model == Model.Membership) {
             model = Model.Burn;
         } else if (model == Model.Free) {
-            model = Model.Lockup;
+            model = Model.Membership;
         }
     }
 }
