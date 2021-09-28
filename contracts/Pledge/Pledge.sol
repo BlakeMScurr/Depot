f9113bf02c055918346691b4d873e78472fdf4e8
blakemscurr <blakemscurr@gmail.com>
2021-09-29 12:55:14 +1300

    style: add natspec comments pledge library

--- contracts/Pledge/Pledge.sol
+++ contracts/Pledge/Pledge.sol
@@ -4,9 +4,16 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
+/**
+ * @title ECDSA Signature Verification
+ * @dev Verifies signatures for requests and responses.
+ */
 library Pledge {
     using ECDSA for bytes32;
 
+    /**
+     * @dev Request made to the server.
+     */
     struct Request {
         bytes meta;
         bytes message;
@@ -15,17 +22,29 @@ library Pledge {
         bytes signature;
     }
 
+    /**
+    * @dev Receipt returned from the server in response to a request.
+    */
     struct Receipt {
         Request request;
         bytes response;
         bytes signature;
     }
 
-    function requireValidServerSignature(Receipt memory resp, address signer) public pure {
-        bytes32 hash = keccak256(abi.encode(resp.request, resp.response));
-        require(hash.toEthSignedMessageHash().recover(resp.signature) == signer, "Server signature must be valid");
+    /**
+    * @dev Verifies a server's receipt, reverts if invalid.
+    * @param receipt Receipt to verify.
+    */
+    function requireValidServerSignature(Receipt memory receipt, address signer) public pure {
+        bytes32 hash = keccak256(abi.encode(receipt.request, receipt.response));
+        require(hash.toEthSignedMessageHash().recover(receipt.signature) == signer, "Server signature must be valid");
     }
 
+    /**
+     * @dev Verifies a user's signature,
+     * @param rq Request to verify.
+     * @return True if the signature is valid, false otherwise.
+     */
     function validUserSignature(Request memory rq) public pure returns (bool) {
         bytes32 hash = keccak256(abi.encode(rq.meta, rq.message, rq.user, rq.blockNumber));
         return hash.toEthSignedMessageHash().recover(rq.signature) == rq.user;
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract Depot is ERC20Capped {
-    constructor() ERC20Capped(1000000) ERC20("Depot", "DEPOT") {}
+contract Silo is ERC20Capped {
+    constructor() ERC20Capped(1000000000000) ERC20("Silo", "SILO") {}
 }
