a32f327ea702737773c79837d216458d059b97b4
blakemscurr <blakemscurr@gmail.com>
2021-09-29 16:19:42 +1300

    refactor: split pledge logic from bond
    Create a new adjudicator that combines bond and pledge logic.
    This allows the bond to be purely about implementing slashing, and
    drawing payment. It also means we can create new adjudicators for new
    pledge combinations, which means our pledge logic doesn't have to be
    generic.

---
+++ contracts/Adjudicator.sol
@@ -0,0 +1,46 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "./Bond.sol";
+import "./Pledge/LivelinessPledge.sol";
+import "./Pledge/RelayPledge.sol";
+
+/**
+ * @title The Adjudicator keeps a server lively and honest.
+ * @dev Using the adjudicator, users can force the server to store and relay
+ * messages. If 
+ */
+contract Adjudicator is Bond {
+    LivelinessPledge livelinessPledge;
+    RelayPledge relayPledge;
+
+    constructor(address erc20, LivelinessPledge _livelinessPledge, RelayPledge _relayPledge) Bond(erc20){
+        livelinessPledge = _livelinessPledge;
+        relayPledge = _relayPledge;
+    }
+
+    /**
+     * @dev Slash 5% of the bond if the server isn't lively
+     */
+    mapping(bytes32 => bool) guiltlyLivelyVerdicts;
+    function notLively(bytes32 requestHash) public {
+        if (!guiltyLivelyVerdicts[requestHash] && livelinessPledge.isBroken(requestHash)) {
+            this.slash(20);
+            guiltlyLivelyVerdicts[requestHash] = true;
+        }
+    }
+
+    /**
+    * @dev Slash 20% of the bond if the server isn't honest
+    * i.e., it lies by ommiting to relay a store request
+    */
+    mapping(bytes32 => bool) guiltyRelayVerdicts;
+    function notHonest(Pledge.Receipt memory storeReceipt, Pledge.Receipt memory findReceipt) public {
+        bytes32 requestHash = keccak256(abi.encodePacked(storeReceipt.request, findReceipt.request));
+        if (!guiltyRelayVerdicts[requestHash] && relayPledge.isBroken(storeReceipt, findReceipt)) {
+            this.slash(5);
+            guiltyRelayVerdicts[requestHash] = true;
+        }
+    }
+}
--- contracts/Bond.sol
+++ contracts/Bond.sol
@@ -4,17 +4,15 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./Pledge/LivelinessPledge.sol";
-import "./Pledge/IPledge.sol";
 
 /**
-* Bond locks up the server's token, slashes it if the server misbehaves, and gives the server a monthly income.
+* @title Server's locked up tokens
+* @dev Bond locks up the server's token, slashes it if the server misbehaves, and gives the server a monthly income.
 */
 contract Bond is Ownable {
     IERC20 erc20;
     uint256 lastDrawdown;
 
-    committedPledge[] pledges; // pledges the server has committed
     mapping(bytes32 => bool) perjuries; // cases of broken pledges
 
     constructor(address _erc20) {
@@ -23,16 +21,16 @@ contract Bond is Ownable {
     }
 
     /**
-    * Anyone can lock up token in the bond as a reward for the server
+    * @dev Lock up token as a reward for the server.
     */
-    function lock(uint256 amount) public {
+    function lock(uint256 amount) external {
         erc20.transferFrom(msg.sender, address(this), amount);
     }
 
     /**
-    * Every month the server can optionally draw a 200th of the locked funds (~%5.8 per year)
+    * @dev Every month the server can optionally draw up to a 200th of the locked funds (~%5.8 per year),
     */
-    function draw(uint256 amount) public onlyOwner {
+    function draw(uint256 amount) external onlyOwner {
         require(amount < erc20.balanceOf(address(this)) / 200, "You can only draw funds less than our monthly drawdown");        
         require(lastDrawdown + 172800 < block.number, "You must wait a month to withdraw your monthly allowance"); // blocks/month = 4 * 60 * 24 * 30 = 172800
         lastDrawdown = block.number;
@@ -40,34 +38,18 @@ contract Bond is Ownable {
     }
 
     /**
-    * The server can freely make pledges on the same bond, it doesn't affect the existing pledges
-    */
-    function addPledge(committedPledge memory p) public onlyOwner {
-        pledges.push(p);
-    }
-
-    /**
-    * If anyone finds that the server has broken a pledge, they can provide proof in the form of receipts, recieve a reward
-    * and slash burn some of the bond in the process.
+    * @dev A user can slash the bond if the server misbehaves.
+    *
+    * To be used by the parent contracts, depending on its pledges.
+    *
     * The amount burned is 200 times the reward so that even if the server repeatedly called slash to recieve its bond early
     * via the reward, it would only end up with approximately its monthly income.
+    *
+    * @param burn amount The amount of tokens to be burned.
     */
-    function slash(Pledge.Receipt[] memory rqs, uint256 i) public {
-        committedPledge memory p = pledges[i];
-        bytes32 hash = keccak256(abi.encode(rqs, i));
-        if (p.pledge.isBroken(rqs) && !perjuries[hash] && p.lastSlash + p.reprieve < block.number) {
-            perjuries[hash] = true;
-            p.lastSlash = block.number;
-            uint256 amountLocked = erc20.balanceOf(address(this));
-            erc20.transfer(msg.sender, amountLocked / (p.burned * 200));
-            erc20.transfer(address(0), amountLocked / p.burned);
-        }
-    }
-
-    struct committedPledge {
-        IPledge pledge; // contract specifying the pledge logic
-        uint256 burned; // fraction of the bond burned if the pledge is broken
-        uint256 reprieve; // minimum blocks between slashes, to give the server operators time to respond before the bond is drained
-        uint256 lastSlash; // block number of the last slashing event
+    function slash(uint256 burn) internal {
+        uint256 amountLocked = erc20.balanceOf(address(this));
+        erc20.transfer(msg.sender, amountLocked / burn / 200);
+        erc20.transfer(address(0), amountLocked / burn);
     }
 }
