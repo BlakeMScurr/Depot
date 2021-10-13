5909402346e92b2ad1387728b33a23dd96ef304b
blakemscurr <blakemscurr@gmail.com>
2021-10-13 15:38:52 +1300

    feat: reject outdated requests

--- server/logic.ts
+++ server/logic.ts
@@ -4,11 +4,16 @@ import * as lpArtifact from "../artifacts/contracts/Pledge/LivelinessPledge.sol/
 import { Request } from '../client/Requests';
 
 const lp = new ethers.utils.Interface(lpArtifact.abi);
-export function handleRequest(siloRequest: any) {
+export async function handleRequest(siloRequest: any, provider: ethers.providers.Provider) {
     lp.encodeFunctionData("request", [siloRequest]);
     let r: Request = siloRequest
 
     if (r.recoverSigner() != r.user) {
         throw new Error("Invalid signature")
+    }
+
+    let bn = await provider.getBlockNumber()
+    if (r.blockNumber < bn) {
+        throw new Error("Enforcement period for offchain request must start in the future")
     }
 }
--- test/server/server.test.ts
+++ test/server/server.test.ts
@@ -1,4 +1,8 @@
-import { expect } from "chai";
+import * as chai from "chai";
+let expect = chai.expect
+import * as chaiAsPromised from "chai-as-promised";
+chai.use(chaiAsPromised.default)
+
 import { ethers } from "hardhat";
 import { handleRequest } from "../../server/logic";
 import { newRequest } from "../../client/Requests"
@@ -6,30 +10,40 @@ import * as e from "ethers";
 
 describe("Server", () => {
     let signers: e.Signer[];
+    let provider: e.providers.Provider;
     before(async () => {
         signers = await ethers.getSigners();
+        provider = await ethers.provider;
     })
 
     describe("Validate request", () => {
         it("Should accept valid requests", async () => {
-            let rq = await newRequest(signers[0], "meta", ethers.utils.toUtf8Bytes("message"), 5)
-            expect(() => {
-                handleRequest(rq)
-            }).not.to.throw()
+            let bn = await ethers.provider.getBlockNumber()
+            let rq = await newRequest(signers[0], "meta", ethers.utils.toUtf8Bytes("message"), bn)
+            await expect(handleRequest(rq, provider)).not.to.be.rejected
         })
 
         it("Should reject invalid types", async () => {
-            expect(() => { handleRequest(9)}).to.throw()
-            expect(() => { handleRequest({ some: "some", junk: "junk"})}).to.throw()
+            await expect(handleRequest(9, provider)).to.be.rejected
+            await expect(handleRequest({ some: "some", junk: "junk"}, provider)).to.be.rejected
             let rq: any = await newRequest(signers[0], "meta", ethers.utils.toUtf8Bytes("message"), 5)
             rq.blockNumber = "bn"
-            expect(() => { handleRequest(rq)}).to.throw()
+            await expect(handleRequest(rq, provider)).to.be.rejected
         })
 
         it("Should reject invalid signatures", async () => {
-            let rq = await newRequest(signers[0], "meta", ethers.utils.toUtf8Bytes("message"), 5)
+            let bn = await ethers.provider.getBlockNumber()
+            let rq = await newRequest(signers[0], "meta", ethers.utils.toUtf8Bytes("message"), bn)
             rq.signature = await signers[0].signMessage(ethers.utils.toUtf8Bytes("asdf"))
-            expect(() => { handleRequest(rq)}).to.throw("Invalid signature")
+            await expect(handleRequest(rq, provider)).to.be.rejected
+        })
+        
+        it("Should reject outdated requests", async() => {
+            await ethers.provider.send("evm_mine", [])
+            let bn = await ethers.provider.getBlockNumber()
+            let rq = await newRequest(signers[0], "meta", ethers.utils.toUtf8Bytes("message"), bn-1)
+            await expect(handleRequest(rq, provider)).to.be.rejectedWith("Enforcement period for offchain request must start in the future")
+
         })
     })
 });
