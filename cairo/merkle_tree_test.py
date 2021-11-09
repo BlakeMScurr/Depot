3973b887b559aee98c9a6a7662e0c65ecc75ea8b
blakemscurr <blakemscurr@gmail.com>
2021-11-09 15:33:29 +1300

    feat: generate pre proof pedersen merkle tree

--- cairo/merkle_tree_test.py
+++ cairo/merkle_tree_test.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+from pre_proof import merkle_tree, merkle_layer
 
 from starkware.crypto.signature.signature import pedersen_hash
 from starkware.starknet.testing.starknet import Starknet
@@ -37,6 +38,7 @@ async def test_merkle_layer():
     assert (await contract.merkle_layer([1,2,3,4,5,6,7,8,9], 0).invoke()).result == (5,)
     assert (await contract.val_in_next_layer([1,2,3,4,5,6,7,8,9], 3).invoke()).result == (pedersen_hash(7,8),)
     assert (await contract.val_in_next_layer([1,2,3,4,5,6,7,8,9], 4).invoke()).result == (9,)
+    assert merkle_layer([1,2,3,4,5,6,7,8,9]) == [pedersen_hash(1,2), pedersen_hash(3,4), pedersen_hash(5,6), pedersen_hash(7,8), 9]
 
 @pytest.mark.asyncio
 async def test_merkle_tree():
@@ -49,18 +51,21 @@ async def test_merkle_tree():
     assert (await contract.merkle_tree([]).invoke()).result == (0,)
     assert (await contract.merkle_tree([1]).invoke()).result == (1,)
     assert (await contract.merkle_tree([1,2]).invoke()).result == (pedersen_hash(1,2),)
-    assert (await contract.merkle_tree([1,2,3,4]).invoke()).result == (
+    assert merkle_tree([1,2]) == pedersen_hash(1,2)
+
+    merkleTree1to4 = (await contract.merkle_tree([1,2,3,4]).invoke()).result.root_hash
+    assert merkleTree1to4  == pedersen_hash(
+        pedersen_hash(1,2),
+        pedersen_hash(3,4)
+    )
+    assert merkleTree1to4 == merkle_tree([1,2,3,4])
+
+    merkleTree1to5 = (await contract.merkle_tree([1,2,3,4,5]).invoke()).result.root_hash
+    assert merkleTree1to5 == pedersen_hash(
         pedersen_hash(
             pedersen_hash(1,2),
             pedersen_hash(3,4)
-        )
-    ,)
-    assert (await contract.merkle_tree([1,2,3,4,5]).invoke()).result == (
-        pedersen_hash(
-            pedersen_hash(
-                pedersen_hash(1,2),
-                pedersen_hash(3,4)
-            ),
-            5
-        )
-    ,)
+        ),
+        5
+    )
+    assert merkleTree1to5 == merkle_tree([1,2,3,4,5])
---
+++ cairo/pre_proof.py
@@ -0,0 +1,21 @@
+from starkware.crypto.signature.signature import pedersen_hash
+
+# TODO: output intermediate hashes for storage
+def merkle_tree(leaves):
+    nextLayer = merkle_layer(leaves)
+    if len(nextLayer) == 0:
+        return 0
+    elif len(nextLayer) == 1:
+        return nextLayer[0]
+    else:
+        return merkle_tree(nextLayer)
+
+def merkle_layer(leaves):
+    nextLayer = []
+    for i in range(0, len(leaves), 2):
+        if i + 1 < len(leaves):
+            nextLayer.append(pedersen_hash(leaves[i], leaves[i+1]))
+        else:
+            nextLayer.append(leaves[i])
+    print("nextLayer:", nextLayer)
+    return nextLayer
