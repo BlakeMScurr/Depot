3d983049a169727c69b89afb7f63f42e499504c1
blakemscurr <blakemscurr@gmail.com>
2021-11-09 15:06:31 +1300

    feat: cairo merkle tree

--- cairo/merkle_tree.cairo
+++ cairo/merkle_tree.cairo
@@ -15,6 +15,20 @@ func val_in_next_layer{pedersen_ptr : HashBuiltin*}(elems_len: felt, elems: felt
     return ([elems + elems_len + index + 1]) # +1 accounts for the memory cell after the array assigned to 0
 end
 
+@external
+func merkle_tree{pedersen_ptr : HashBuiltin*}(elems_len: felt, elems: felt*) -> (root_hash):
+    let (next_len) = merkle_layer(elems_len, elems, 0)
+    if next_len == 0:
+        return (0) # we define 0 as the hash of an empty tree
+    end
+    if next_len == 1:
+        return ([elems + elems_len + 1])
+    end
+
+    let (res) = merkle_tree(next_len, elems + elems_len + 1)
+    return (res)
+end
+
 # Computes the next layer of a merkle tree
 # elem is a pointer to the first element of the layer
 # length is the number of elements in the layer
@@ -30,7 +44,7 @@ func merkle_layer{pedersen_ptr : HashBuiltin*}(elems_len: felt, elems: felt*, ha
     # @guthl, it seems like the element at elems + elems_len is set to 0, even though there is not a zero element explicitly set there,
     # is this some kind of terminus? We add 1 to get to the first unassigned memory cell.
     # TODO: try to remove the +1
-    let hash_offset = elems+elems_len+hash_index+1
+    let hash_offset = elems + elems_len + 1 + hash_index
     if 2*hash_index+1 == elems_len:
         assert [hash_offset] = [elems+elems_len-1]
         return (1)
--- cairo/merkle_tree_test.py
+++ cairo/merkle_tree_test.py
@@ -9,15 +9,9 @@ CONTRACT_FILE = os.path.join(
     os.path.dirname(__file__), "merkle_tree.cairo")
 
 
-# The testing library uses python's asyncio. So the following
-# decorator and the ``async`` keyword are needed.
 @pytest.mark.asyncio
 async def test_merkle_layer():
-    # Create a new Starknet class that simulates the StarkNet
-    # system.
     starknet = await Starknet.empty()
-
-    # Deploy the contract.
     contract = await starknet.deploy(
         source=CONTRACT_FILE,
     )
@@ -43,3 +37,30 @@ async def test_merkle_layer():
     assert (await contract.merkle_layer([1,2,3,4,5,6,7,8,9], 0).invoke()).result == (5,)
     assert (await contract.val_in_next_layer([1,2,3,4,5,6,7,8,9], 3).invoke()).result == (pedersen_hash(7,8),)
     assert (await contract.val_in_next_layer([1,2,3,4,5,6,7,8,9], 4).invoke()).result == (9,)
+
+@pytest.mark.asyncio
+async def test_merkle_tree():
+    starknet = await Starknet.empty()
+    contract = await starknet.deploy(
+        source=CONTRACT_FILE,
+    )
+
+    # TODO: handle the trivial case of an empty tree - currently panics
+    assert (await contract.merkle_tree([]).invoke()).result == (0,)
+    assert (await contract.merkle_tree([1]).invoke()).result == (1,)
+    assert (await contract.merkle_tree([1,2]).invoke()).result == (pedersen_hash(1,2),)
+    assert (await contract.merkle_tree([1,2,3,4]).invoke()).result == (
+        pedersen_hash(
+            pedersen_hash(1,2),
+            pedersen_hash(3,4)
+        )
+    ,)
+    assert (await contract.merkle_tree([1,2,3,4,5]).invoke()).result == (
+        pedersen_hash(
+            pedersen_hash(
+                pedersen_hash(1,2),
+                pedersen_hash(3,4)
+            ),
+            5
+        )
+    ,)
